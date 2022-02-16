9b67ea59963b66e9e557124a593b4dbbd3db554a
brenolevi <brenolevi@ymail.com>
2022-02-16 14:35:39 -0300

    Faster solution by reversing the order of permutations and returning the first valid result

--- solutions/p068.py
+++ solutions/p068.py
@@ -1,16 +1,18 @@
 from itertools import permutations
 
-max_string = ""
 
-for p in permutations(reversed(range(1, 10))):
-    e = list(p[:4]) + [10]  # 10 should be external due to 16-digit restriction
-    e = e[e.index(min(e)):] + e[:e.index(min(e))]  # rotate external ring to start at minimum value
-    for r in permutations(p[4:]):
-        for i in range(5):
-            if e[i] + r[i] != e[(i + 1) % 5] + r[(i + 2) % 5]:
-                break
-        else:
-            candidate_string = "".join([str(100*e[i] + 10*r[i] + r[(i + 1)%5]) for i in range(5)])
-            if candidate_string > max_string:
-                max_string = candidate_string
-print(max_string)
+def calculate_max_string():
+    for p in permutations(reversed(range(1, 10))):
+        e = list(p[:4]) + [10]  # 10 should be external due to 16-digit restriction
+        e = e[e.index(min(e)):] + e[:e.index(min(e))]  # rotate external ring to start at minimum value
+        for r in permutations(p[4:]):
+            for i in range(5):
+                if e[i] + r[i] != e[(i + 1) % 5] + r[(i + 2) % 5]:
+                    break
+            else:
+                return "".join([str(100*e[i] + 10*r[i] + r[(i + 1) % 5]) for i in range(5)])
+
+
+if __name__ == "__main__":
+    print(calculate_max_string())
+    print(None)
