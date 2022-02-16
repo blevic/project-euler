b348a421789c635a2ffa7d0b33a5ebfdeedf046f
brenolevi <brenolevi@ymail.com>
2022-02-16 14:27:00 -0300

    Bug fix

--- solutions/p068.py
+++ solutions/p068.py
@@ -5,7 +5,7 @@ max_string = ""
 for p in permutations(reversed(range(1, 10))):
     e = list(p[:4]) + [10]  # 10 should be external due to 16-digit restriction
     e = e[e.index(min(e)):] + e[:e.index(min(e))]  # rotate external ring to start at minimum value
-    for r in itertools.permutations(p[4:]):
+    for r in permutations(p[4:]):
         for i in range(5):
             if e[i] + r[i] != e[(i + 1) % 5] + r[(i + 2) % 5]:
                 break
